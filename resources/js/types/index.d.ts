b5cf05b471ce547629f7eab0165beaaae11cda83
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2025-05-09 00:27:40 +0700

    feat: Add Pagination type for handling paginated data responses

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -41,3 +41,14 @@ export interface User {
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
 }
+
+export type Pagination<T> = {
+    data: T[];
+    current_page: number;
+    last_page: number;
+    links: {
+        url: string | null;
+        label: string;
+        active: boolean;
+    }[];
+};
